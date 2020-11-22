e919031a29205d048ad10037e345e0c477ebccb1
jungjinhwan0101 <jungjinhwan0101@gmail.com>
2020-11-22 16:32:37 +0900

    trivial

--- user/domain/services.py
+++ user/domain/services.py
@@ -2,20 +2,20 @@ from user.domain.repositories import UserDataBaseRepository
 
 
 class UserService:
-    def __init__(self, repository):
-        self.repository = repository
+    def __init__(self, db_repo):
+        self.db_repo = db_repo
 
     def create_user(self, username, password=None):
-        repo_user = self.repository.create(username, password)
+        repo_user = self.db_repo.create(username, password)
         return repo_user
 
     def find_by_id(self, user_id):
-        repo_user = self.repository.find(filters={'id': user_id}).get()
+        repo_user = self.db_repo.find(filters={'id': user_id}).get()
         return repo_user
 
     def find_by_username(self, username):
-        repo_user = self.repository.find(filters={'username': username}).get()
+        repo_user = self.db_repo.find(filters={'username': username}).get()
         return repo_user
 
 
-user_service = UserService(UserDataBaseRepository)
+user_service = UserService(db_repo=UserDataBaseRepository)
