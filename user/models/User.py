1757e951ba6b6728711511df6603844a837906b1
jungjinhwan0101 <jungjinhwan0101@gmail.com>
2020-11-22 16:43:54 +0900

    User > models 관련 패키지화

--- user/models/User.py
+++ user/models/User.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.utils.translation import gettext_lazy as _
-from user.managers import ORMUserManager
+from user.models.managers import ORMUserManager
 
 
 class ORMUser(AbstractBaseUser):
